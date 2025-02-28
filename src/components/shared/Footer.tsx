88a9e9d354fe92bb5cb5e27fba8b50d2dde6e6e5
bibhashhalder <halderbibhash80@gmail.com>
2025-02-28 08:43:59 +0600

    footer added

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-black h-24'>
-            <h1>This is Footer</h1>
+        <div className='bg-black h-24 items-center flex justify-center'>
+            <h1 className='text-white text-center '>&copy; developed by bibhash halder //<span className='text-gray-500 text-lg'>2025</span> </h1>
         </div>
     );
 };
